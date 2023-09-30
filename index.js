ca0973474730158e7103af44ce8d012e3c08bb38
Anukriti Thapa <anukritithapa54@gmail.com>
2023-09-30 20:04:08 +0545

    learned about variables

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 console.log("Hello from index.js");
 // REPL (Read,Evaluate,Print,Loop)
+
+//ES5
+var person="Anukriti"
+person="Anukriti Thapa"
+
+
+console.log(person);
+
+//ES6
+/*
+Let:changeable variable
+const: constant (which never chnages)
+*/
+
+let p="ANU";
+p="Kriti";
+console.log(p);
+
+const per="Anubhav"
+console.log(per);
+
+const pi="3.14";
+console.log(pi);
